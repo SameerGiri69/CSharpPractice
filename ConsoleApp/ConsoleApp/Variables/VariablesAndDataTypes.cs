f8d3b35825c7d30271d8b3a105953bdf29d5beeb
SameerGiri <sameergiri153000@gmail.com>
2023-09-21 15:05:24 +0545

    Practiced Types of variables

--- ConsoleApp/ConsoleApp/Variables/VariablesAndDataTypes.cs
+++ ConsoleApp/ConsoleApp/Variables/VariablesAndDataTypes.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Variables
 {
-    internal class VariablesAndDataTypes
+    public class VariablesAndDataTypes
     {
+        public int a;
+        string name;
+        
+        public void StudentInformation()
+        {
+            a = 12;
+            name = "sameer";
+            float gpa = 3.5f;
+            bool passed = true;
+            long LCno = 00200001656;
+        }
+       
+    }
+    public class Student
+    {
+        VariablesAndDataTypes mydata = new VariablesAndDataTypes();
+        public void accessedInformation()
+        {
+            Console.WriteLine(mydata.StudentInformation);
+        }
+        
     }
 }
