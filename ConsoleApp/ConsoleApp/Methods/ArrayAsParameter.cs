6e66255c72a70ddb30ee171f5a12a89342602939
SameerGiri <sameergiri153000@gmail.com>
2023-09-22 14:35:19 +0545

    Practiced different types of methods

--- ConsoleApp/ConsoleApp/Methods/ArrayAsParameter.cs
+++ ConsoleApp/ConsoleApp/Methods/ArrayAsParameter.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.Methods
 {
-    internal class ArrayAsParameter
+    public class ArrayAsParameter
     {
+        public int ArrayAddition(int[] expenses)
+        {
+            int sum = 0;
+            foreach (var item in expenses)
+            {
+                sum += item;
+
+            }
+            return sum;
+        }
+        public void displayArrayAddition()
+        {
+            int[] expenses = { 1000, 2000, 6585, 8945 };
+            int result = ArrayAddition(expenses);
+            Console.WriteLine(result);
+        }
     }
 }
---
+++ ConsoleApp/ConsoleApp/Methods/MethodOverloading.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Methods
+{
+    public class MethodOverloading
+    {
+        //using same method names multiple times but with different parameters
+        public string Information(int a, int b)
+        {
+            int sum = a + b;
+            string info = "the total sum is : " + sum;
+            return info;
+        }
+        public string Information(string name, string address)
+        {
+            string info = "the name is: " + name + " and their address is : " + address;
+            return info;
+        }
+        public void viewInformaiton()
+        {
+            Console.WriteLine(Information(1, 2));
+            Console.WriteLine(Information("sameer", "jamunkhadi"));
+        }
+    }
+}
