8f8955f190ac9f8da16ff35b6c421983ff24f5d6
SameerGiri <sameergiri153000@gmail.com>
2023-10-20 21:16:26 +0545

    Interfaces, Abstract class practice

--- ConsoleApp/ConsoleApp/Interfaces/IinterfacePractice.cs
+++ ConsoleApp/ConsoleApp/Interfaces/IinterfacePractice.cs
@@ -4,22 +4,37 @@ public interface IInterfacePractice
 {
     void sound();
 }
+
+public interface Life
+{
+    void AliveOrNot();
+}
 // a class must implement methods and properties set by the interface else there'll be compile errors
 
 
-   public class Car : IInterfacePractice
+public class Car : IInterfacePractice, Life
     {
          public void sound()
         {
             Console.WriteLine("vrrrr vrrrr");
         }
+
+         public void AliveOrNot()
+         {
+             Console.WriteLine("The car is a non living thing");
+         }
     }
 
-   public class Animal : IInterfacePractice
+   public class Animal : IInterfacePractice, Life
    {
        public void sound()
        {
            Console.WriteLine("meoww");
+       }
+
+       public void AliveOrNot()
+       {
+           Console.WriteLine("Animal is a living thing");
        }
    }
     
